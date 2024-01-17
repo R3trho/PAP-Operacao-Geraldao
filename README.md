b1306b5af8679718e354c71fc769ed6be474ccf9
Fernando Reis <97308587+R3trho@users.noreply.github.com>
2024-01-17 11:06:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PAP OperacaoGeraldao
+ Como projeto para a PAP, fiz o "Operação Geraldão", é o nome do jogo que criei, em Godot Engine,, consiste em ser um jogo RPG em que o protagonista, Geraldão, vai salvar o seu amigo Aníbal, após ter sido raptado pelo Culto Malvadão e o seu Líder FAZE
---
+++ pap/script/player.gd
@@ -0,0 +1,68 @@
+extends CharacterBody2D
+
+const SPEED = 35.0
+var current_dir = "none"
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+	move_and_slide()
+
+@warning_ignore("unused_parameter")
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.y = SPEED
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.y = -SPEED
+		velocity.x = 0
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+
+	if dir == "right":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	elif dir == "left":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	elif dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
+	elif dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
