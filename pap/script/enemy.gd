a88621547b863378652502ce126f66cecdfa96df
Fernando Reis <97308587+R3trho@users.noreply.github.com>
2024-02-02 20:34:05 +0000

    Fixed enemy ysorting

--- pap/script/enemy.gd
+++ pap/script/enemy.gd
@@ -18,6 +18,7 @@ func _physics_process(delta):
 			
 	else:
 		$AnimatedSprite2D.play("Idle")
+	Move(delta)
 
 func _on_detection_area_body_entered(body):
 	player = body
@@ -30,3 +31,9 @@ func _on_detection_area_body_exited(body):
 
 func enemy():
 	pass
+
+func Move(delta):
+
+	if (player):
+		var Goto = (player.position - position).normalized()
+		move_and_slide()
