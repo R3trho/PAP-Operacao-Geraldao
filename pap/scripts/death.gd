fd736dd0c3951fc639747d7e0cb74f8c00ffcb41
Fernando Reis <97308587+R3trho@users.noreply.github.com>
2024-04-17 22:10:58 +0100

    OPERAÇÃO GERALDÃO BETA V1

--- pap/scripts/death.gd
+++ pap/scripts/death.gd
@@ -3,12 +3,10 @@ extends Control
 var is_paused = false
 
 func Restart():
-	TogglePaused()
 	get_tree().reload_current_scene()
 	
 	
 func Exit():
-	TogglePaused()
 	get_tree().change_scene_to_file("res://scenes/maincode.tscn")
 
 
@@ -20,11 +18,3 @@ func TogglePaused():
 	self.is_paused = is_paused
 	
 	visible = is_paused
-
-
-func _on_restart_pressed():
-	get_tree().reload_current_scene()
-
-
-func _on_quit_pressed():
-	get_tree().quit()
--- pap/scripts/player.gd
+++ pap/scripts/player.gd
@@ -9,12 +9,14 @@ var attack_ip = false
 const SPEED = 40.0
 var current_dir = "none"
 
+var attack_one = false
+
 @onready var death_var = $CanvasLayer2/Death
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
-func _physics_process(delta):
+func _process(delta):
 	player_movement(delta)
 	move_and_slide()
 	enemy_attack()
@@ -22,7 +24,8 @@ func _physics_process(delta):
 	
 	if health <= 0:
 		print("Player Defeated")
-		death()
+		$"AnimatedSprite2D".visible = false
+		$"CanvasLayer2/Death".visible = true
 		health = 100
 
 @warning_ignore("unused_parameter")
@@ -56,38 +59,35 @@ func player_movement(delta):
 func play_anim(movement):
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
-
-	if dir == "right":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("side_idle")
-	elif dir == "left":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("side_idle")
-	elif dir == "down":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("front_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("front_idle")
-	elif dir == "up":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("back_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("back_idle")
-
-func player():
-	pass
+	if !attack_one:
+		if dir == "right":
+			anim.flip_h = true
+			if movement == 1:
+				anim.play("side_walk")
+			elif movement == 0:
+				if attack_ip == false:
+					anim.play("side_idle")
+		elif dir == "left":
+			anim.flip_h = false
+			if movement == 1:
+				anim.play("side_walk")
+			elif movement == 0:
+				if attack_ip == false:
+					anim.play("side_idle")
+		elif dir == "down":
+			anim.flip_h = true
+			if movement == 1:
+				anim.play("front_walk")
+			elif movement == 0:
+				if attack_ip == false:
+					anim.play("front_idle")
+		elif dir == "up":
+			anim.flip_h = false
+			if movement == 1:
+				anim.play("back_walk")
+			elif movement == 0:
+				if attack_ip == false:
+					anim.play("back_idle")
 
 
 func _on_player_hitbox_body_entered(body):
@@ -115,27 +115,33 @@ func _on_attack_cooldown_timeout():
 
 func attack():
 	var dir = current_dir
-	
 	if Input.is_action_just_pressed("attack"):
 		global.player_current_attack = true
 		attack_ip = true
+		attack_one = true
 		if dir == "right":
 			$AnimatedSprite2D.flip_h = false
 			$AnimatedSprite2D.play("side_attack")
 			$deal_attack_timer.start()
-		if dir == "left":
+		elif dir == "left":
 			$AnimatedSprite2D.flip_h = true
 			$AnimatedSprite2D.play("side_attack")
 			$deal_attack_timer.start()
-		if dir == "down":
+		elif dir == "down":
 			$AnimatedSprite2D.play("front_attack")
 			$deal_attack_timer.start()
-		if dir == "up":
+		elif dir == "up":
 			$AnimatedSprite2D.play("front_attack")
 			$deal_attack_timer.start()
+		attack_one = false
 
-
+##Isto não tá a correr bem
+##Desculpa eu sou burro 👌
 func _on_deal_attack_timer_timeout():
+	print ("asd")
 	$deal_attack_timer.stop()
-	global.player_current_attack == false
+	global.player_current_attack = false
 	attack_ip = false
+	
+func player():
+	pass
